5c1a3be3a59701d4752189b06943d1d520019564
zhanghr <1044235759@qq.com>
2017-04-12 13:40:02 +0800

    no message

--- JPF_Proj/src/main/SimulateJPFTest.java
+++ JPF_Proj/src/main/SimulateJPFTest.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 
 public class SimulateJPFTest {
 	public static void main(String[] args) {
-		String targetClass = "example.HelloWorld";//"org.apache.commons.math.stat.descriptive.DescriptiveStatistics";
-		String method = "example.HelloWorld.main(sym)";//"org.apache.commons.math.stat.descriptive.DescriptiveStatistics.addValue(sym)";
+		String targetClass = "PushPop";//"org.apache.commons.math.stat.descriptive.DescriptiveStatistics";
+		String method = "PushPop.push_pop(sym#sym#sym)";//"org.apache.commons.math.stat.descriptive.DescriptiveStatistics.addValue(sym)";
 		String classPath = "bin";
 		String[] appArgs = {
 				"+classpath="+classPath,
@@ -21,19 +21,20 @@ public class SimulateJPFTest {
 				"+symbolic.max_int=200",
 				"+symbolic.min_double=-2000.0",
 				"+symbolic.max_double=2000.0",
+				"+symbolic.debug=on",
 				"+search.class=.search.heuristic.BFSHeuristic",
 				"+listener=gov.nasa.jpf.symbc.sequences.SymbolicSequenceListener"
 				};
 //		new Test().runJPF(new String[]{"+app=src/main/addValue.jpf"});
 		SimulateJPF.runJPF(appArgs);
 		System.out.println("======================================================================");
-		testCoverage();
+//		testCoverage();
 	}
 
 	private static void testCoverage() {
 
-		String targetClass = "example.TestExample";//"org.apache.commons.math.stat.descriptive.DescriptiveStatistics";
-		String method = "example.TestExample.main(sym)";//"org.apache.commons.math.stat.descriptive.DescriptiveStatistics.addValue(sym)";
+		String targetClass = "org.apache.commons.math.stat.descriptive.DescriptiveStatistics";
+		String method = "org.apache.commons.math.stat.descriptive.DescriptiveStatistics.addValue(sym)";
 		String classPath = "bin";
 		String[] appArgs = {
 				"+classpath="+classPath,
@@ -48,8 +49,8 @@ public class SimulateJPFTest {
 				"+symbolic.max_double=2000.0",
 				"+search.class=.search.heuristic.BFSHeuristic",
 				"+listener=gov.nasa.jpf.symbc.sequences.SymbolicSequenceListener,.listener.CoverageAnalyzer",
-				"coverage.include=T1,T2",
-				"coverage.show_methods=true"
+				"+coverage.include=T1,T2",
+				"+coverage.show_methods=true"
 				};
 		SimulateJPF.runJPF(appArgs);
 		
